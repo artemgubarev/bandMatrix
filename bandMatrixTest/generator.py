ce63b23a3c9bcd5cf5b4086eee483a5bdcd0d632
artemgubarev <artem.grvz@gmail.com>
2025-02-05 19:54:31 +0300

    generator

--- bandMatrixTest/generator.py
+++ bandMatrixTest/generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def generate_banded_matrix(n, b, low=1, high=10):
     random_values = np.random.randint(low, high, size=(n, 2 * b + 1))
@@ -7,13 +8,28 @@ def generate_banded_matrix(n, b, low=1, high=10):
         start = max(0, i - b)
         end = min(n, i + b + 1)
         matrix[i, start:end] = random_values[i, :end - start]
-
     return matrix 
 
 def generate_random_array(length, min_value=0, max_value=100):
-    return [np.random.randint(min_value, max_value) for _ in range(length)]
+    return [np.random.uniform(min_value, max_value) for _ in range(length)]
 
-n = 4000
-b = 1756
-banded_matrix = generate_banded_matrix(n, b)
-c = generate_random_array(n, min_value=0.1, max_value=10)
+def write_matrix(filename, n, b, matrix, c):
+    lines = [f"{n}\n", f"{b}\n"]
+    lines.extend(' '.join(map(str, row)) + '\n' for row in matrix)
+    lines.append(' '.join(map(str, c)) + '\n')
+    with open(filename, 'w') as f:
+        f.writelines(lines)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 generate_matrix.py <n> <b>")
+        sys.exit(1)
+
+    n = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    filename = f"matrix{n}x{b}.txt"
+    filepath = os.path.join("testData", filename)
+    matrix = generate_banded_matrix(n, b)
+    c = generate_random_array(n, min_value=0.1, max_value=10)
+    write_matrix(filepath, n, b, matrix, c)
