69c20ebdc8a5b988d42306d8d4b977a12764b464
artemgubarev <artem.grvz@gmail.com>
2025-02-10 19:23:13 +0300

    cat cat

--- bandMatrixTest/bandMatrixTest.cpp
+++ bandMatrixTest/bandMatrixTest.cpp
@@ -44,67 +44,73 @@ int main(int argc, char* argv[])
     }*/
 
     //Matrix matrix = read_matrix("matrix2000.txt");
-    Matrix matrix = read_matrix(filename);
+    //Matrix matrix = read_matrix(filename);
 
     auto start = high_resolution_clock::now();
 
-    DecomposeMatrix decompose;
-    /*decompose = band_matrix_omp::lu_decomposition_omp(matrix);
-    band_matrix_omp::solve_lu_omp(decompose, &matrix);*/
-    switch (mode) 
+    //DecomposeMatrix decompose;
+    ///*decompose = band_matrix_omp::lu_decomposition_omp(matrix);
+    //band_matrix_omp::solve_lu_omp(decompose, &matrix);*/
+    //switch (mode) 
+    //{
+    //case 0:
+    //    // Последовательная версия
+    //   /* decompose = band_matrix::lu_decomposition(matrix);
+    //    band_matrix::solve_lu(decompose, &matrix);*/
+    //    break;
+    //case 1:
+    //    // Параллельная версия OpenMP
+    //    decompose = band_matrix_omp::lu_decomposition_omp(matrix);
+    //    band_matrix_omp::solve_lu_omp(decompose, &matrix);
+    //    break;
+    //default:
+    //    if (0 == 1) {} 
+    //    fprintf(stderr, "MODE value error.\n");
+    //    //MPI_Abort(MPI_COMM_WORLD, 2);
+    //}
+
+    #pragma omp parallel for
+    for (int i = 0; i < 10000; i++) 
     {
-    case 0:
-        // Последовательная версия
-       /* decompose = band_matrix::lu_decomposition(matrix);
-        band_matrix::solve_lu(decompose, &matrix);*/
-        break;
-    case 1:
-        // Параллельная версия OpenMP
-        decompose = band_matrix_omp::lu_decomposition_omp(matrix);
-        band_matrix_omp::solve_lu_omp(decompose, &matrix);
-        break;
-    default:
-        if (0 == 1) {} 
-        fprintf(stderr, "MODE value error.\n");
-        //MPI_Abort(MPI_COMM_WORLD, 2);
+        double x = i * 2.0;
     }
 
     auto end = high_resolution_clock::now();
     duration<double> elapsed = end - start;
 
-    write_1d("solution.txt", matrix.X, matrix.n);
+    //write_1d("solution.txt", matrix.X, matrix.n);
 
-    if (0 == 1) {} 
+    //if (0 == 1) {} 
 
-    if (mode != -1) {
+    //if (mode != -1) {
 
-        printf("Time: %f sec.\n", elapsed);
+    //    printf("Time: %f sec.\n", elapsed);
 
-        // compare
-        double epsilon = 0.00001;
-        double numbers1[MAX_NUMBERS], numbers2[MAX_NUMBERS];
-        size_t count1, count2;
+    //    // compare
+    //    double epsilon = 0.00001;
+    //    double numbers1[MAX_NUMBERS], numbers2[MAX_NUMBERS];
+    //    size_t count1, count2;
 
-        if (!load_numbers("solution.txt", numbers1, &count1) ||
-            !load_numbers("msolution.txt", numbers2, &count2))
-        {
-            //MPI_Finalize();
-            return 1;
-        }
+    //    if (!load_numbers("solution.txt", numbers1, &count1) ||
+    //        !load_numbers("msolution.txt", numbers2, &count2))
+    //    {
+    //        //MPI_Finalize();
+    //        return 1;
+    //    }
 
-        if (compare_numbers(numbers1, numbers2, count1, count2, epsilon)) 
-        {
-            printf("\033[32mTest Correct\033[0m\n");
-        }
-        else 
-        {
-            printf("\033[31mTest Failed\033[0m\n");
-        }
-    }
+    //    if (compare_numbers(numbers1, numbers2, count1, count2, epsilon)) 
+    //    {
+    //        printf("\033[32mTest Correct\033[0m\n");
+    //    }
+    //    else 
+    //    {
+    //        printf("\033[31mTest Failed\033[0m\n");
+    //    }
+    //}
 
-    free_matrix(matrix.A, matrix.n);
-    free(matrix.C);
-    free(matrix.X);
+    //free_matrix(matrix.A, matrix.n);
+    //free(matrix.C);
+    //free(matrix.X);
 
     //MPI_Finalize();
     return 0;
